b9f29b61a920d90997256bdde5a0125e1e14e575
zhaopengtian <729560832@qq.com>
2022-02-17 10:44:38 +0800

    增加gvg类

--- TestCases/test.py
+++ TestCases/test.py
@@ -4,12 +4,16 @@ from Utils.tokeinfo import tokeninfo
 class Totasks(unittest.TestCase,Helper):
 
     def test_bd(self):
-        '''BD验证'''
+
         token = tokeninfo()
-        url = 'http://webapi.loex.abc/exchange-web-api/v1/bd/verify'
-        headers = {"Content-Type": "application/json;charset=UTF-8", "exchange-token": token}
-        data = {'type':'Skype', 'content':'Skype'}
-        r = self.post(url,data,headers)
+        url = 'http://192.168.8.197:9007/calorie-management-diet/login/sendMsg?phone=14000000101&templateId=441820&areaCode=0086'
+        headers = {"Content-Type": "application/json;charset=UTF-8", "platform": "Android","version":"1.0.0"}
+        data = {'phone':'14000000103', 'templateId':'441820','areaCode':'0086'}
+        r = self.get(url,headers)
         print(r.text)
         print(r.json()['msg'])
-        self.assertEqual(r.json()['code'], '10003')
+        # self.assertEqual(r.json()['code'], '10003')
+
+
+if __name__ == '__main__':
+    unittest.main
