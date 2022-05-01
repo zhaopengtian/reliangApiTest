a4f3eddee6989121c8e5550c663dba920390d965
zhaopengtian <729560832@qq.com>
2022-05-02 00:44:12 +0800

    修改bd3正确

--- TestCases/test_tasks.py
+++ TestCases/test_tasks.py
@@ -14,11 +14,11 @@ class Totasks(TestApi,Helper):
 
     def test_3_bd1(self):
         '''BD验证'''
-        token = tokeninfo()
-        url = 'http://webapi.loex.abc/exchange-web-api/v1/bd/verify'
-        headers = {"Content-Type": "application/json;charset=UTF-8", "exchange-token": token}
-        data = {'type':'Skype', 'content':'Skype'}
-        r = self.post(url,data,headers)
+        # token = tokeninfo()
+        # url = 'http://webapi.loex.abc/exchange-web-api/v1/bd/verify'
+        # headers = {"Content-Type": "application/json;charset=UTF-8", "exchange-token": token}
+        # data = {'type':'Skype', 'content':'Skype'}
+        # r = self.post(url,data,headers)
         self.assertEqual(r.json()['code'],'10002')
 
 
